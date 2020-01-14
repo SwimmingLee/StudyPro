d4a75131be463c7f943f8599ad340d813362ff55
ungdoong <xorwls124@gmail.com>
2020-01-14 17:42:50 +0900

    Intro, Main

---
+++ front-sk/src/views/Components.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <topBlock></topBlock>
+        <basic-elements></basic-elements>
+    </div>
+</template>
+<script>
+import topBlock from "./components/TopBlock";
+import BasicElements from "./components/BasicElements";
+export default {
+  name: "components",
+  components: {
+    topBlock,
+    BasicElements
+  }
+};
+</script>
---
+++ front-sk/src/views/components/TopBlock.vue
@@ -0,0 +1,46 @@
+<template>
+    <section class="section-hero section-shaped my-0">
+        <div class="shape shape-style-1 shape-default">
+            <span class="span-150"></span>
+            <span class="span-50"></span>
+            <span class="span-50"></span>
+            <span class="span-75"></span>
+            <span class="span-100"></span>
+            <span class="span-75"></span>
+            <span class="span-50"></span>
+            <span class="span-100"></span>
+            <span class="span-50"></span>
+            <span class="span-100"></span>
+        </div>
+        <div class="container shape-container align-items-center">
+            <div class="col px-5">
+                <div class="row justify-content-center align-items-center">
+                    <div class="col-lg-7 text-center pt-lg">
+                        <p class="lead text-white mt-4 mb-5">LEFT ARTICLE. LEFT ARTICLE. LEFT ARTICLE. LEFT ARTICLE. </p>
+                        <div class="btn-wrapper">
+                            <base-button tag="a"
+                                         href="https://demos.creative-tim.com/vue-argon-design-system/documentation"
+                                         class="mb-3 mb-sm-0"
+                                         type="info"
+                                         icon="fa fa-code">
+                                Components
+                            </base-button>
+                            <base-button tag="a"
+                                         href="https://www.creative-tim.com/product/vue-argon-design-system"
+                                         class="mb-3 mb-sm-0"
+                                         type="white"
+                                         icon="ni ni-cloud-download-95">
+                                Download Vue
+                            </base-button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {};
+</script>
+<style>
+</style>
