17fab02fd9442af37cdce6c7f4f023517550bbd4
SwimmingLee <swimming_7291@daum.net>
2020-01-16 12:14:34 +0900

    [feat] modify jwt contents ,

--- back/models/users.js
+++ back/models/users.js
@@ -1,7 +1,8 @@
 /* jshint indent: 2 */
+import crypto from "crypto"
 
-module.exports = function(sequelize, DataTypes) {
-  const users =  sequelize.define('users', {
+module.exports = function (sequelize, DataTypes) {
+  const users = sequelize.define('users', {
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -37,6 +38,31 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     tableName: 'users'
   });
+
+  users.prototype.verify = function(password) {
+    let cipher = crypto.createCipher('aes192', process.env.SECRET_KEY)
+    cipher.update(password, 'utf8', 'base64')
+    const ciphered_password = cipher.final('base64')
+
+    return this.dataValues.password === ciphered_password;
+  };
+
+  users.save = function (user) {
+    const {email, nickname, name, password, gender, auth} = user;
+
+    let cipher = crypto.createCipher('aes192', process.env.SECRET_KEY)
+    cipher.update(password, 'utf8', 'base64')
+    const ciphered_password = cipher.final('base64')
+
+    return this.create({
+      email,
+      name,
+      password: ciphered_password,
+      nickname,
+      gender,
+      auth
+    })
+  }
 
   return users;
 };
