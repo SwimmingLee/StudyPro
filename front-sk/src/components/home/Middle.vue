ff8827305e140755054665a525dbe961212ce3ca
ungdoong <xorwls124@gmail.com>
2020-01-21 08:59:51 +0900

    [FEAT] Main 하단 통계, S02P12A106-27

--- front-sk/src/components/home/Middle.vue
+++ front-sk/src/components/home/Middle.vue
@@ -1,40 +1,23 @@
 <template>
-  <div class="d-block white--text" style="height:600px">
+  <div class="d-block">
     <v-row>
-      <v-card class="mx-auto text-center" color="green" dark max-width="600">
-        <v-card-text>
-          <v-sheet color="rgba(0, 0, 0, .12)">
-            <v-sparkline
-              :value="value"
-              color="rgba(255, 255, 255, .7)"
-              height="100"
-              padding="24"
-              stroke-linecap="round"
-              smooth
-            >
-              <template v-slot:label="item"> ${{ item.value }} </template>
-            </v-sparkline>
-          </v-sheet>
-        </v-card-text>
-
-        <v-card-text>
-          <div class="display-1 font-weight-thin">Sales Last 24h</div>
-        </v-card-text>
-
-        <v-divider></v-divider>
-
-        <v-card-actions class="justify-center">
-          <v-btn block text>Go to Report</v-btn>
-        </v-card-actions>
-      </v-card>
+      <v-col class="col-auto ma-2 ml-5">
+        <span class="display-2">통계</span>
+      </v-col>
+      <v-col class="col-auto ma-2">
+        <span class="display-2">통계</span>
+      </v-col>
+      <v-col class="col-auto ma-2">
+        <span class="display-2">통계</span>
+      </v-col>
+      <v-col class="col-auto ma-2 mr-5">
+        <span class="display-2">통계</span>
+      </v-col>
     </v-row>
   </div>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    value: [423, 446, 675, 510, 590, 610, 760]
-  })
 };
 </script>
