3f82de771daad25541ae64ddf1491646a85d2b0a
김현철 <supergrammer@naver.com>
2020-01-22 17:03:40 +0900

    [Feat] Header Side Menu, S02P12A106-76

---
+++ front-sk/src/components/WorkSpace.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-flex row>
+      <v-flex column>
+        <v-card outlined class="primary pa-2 ma-1">12324</v-card>
+      </v-flex>
+      <v-layout column justify>
+        <v-card outlined class="primary pa-2 ma-1">12324</v-card>
+      </v-layout>
+    </v-flex>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
--- front-sk/src/components/common/Header.vue
+++ front-sk/src/components/common/Header.vue
@@ -1,106 +1,149 @@
 <template>
   <nav>
-    <v-toolbar text>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>
-        <v-btn text to="/home" color="none">
-          <span class="font-weight-light">Study</span>
-          <span>PRO</span>
-        </v-btn>
-      </v-toolbar-title>
-
-        <v-spacer></v-spacer>
-
-      <v-menu offset-y>
-        <template v-slot:activator="{ on }">
-          <v-btn text color="gray" v-on="on">
-            <v-icon>expand_more</v-icon>
-            <span>Menu</span>
-          </v-btn>
+    <!-- Header Menu -->
+    <v-card class="overflow-hidden">
+      <v-app-bar
+        fixed
+        shrink-on-scroll
+        prominent
+        src="@/assets/Background.jpg"
+        fade-img-on-scroll
+        scroll-threshold="200"
+        class="pa-1"
+      >
+        <template v-slot:img="{ props }">
+          <v-img v-bind="props" gradient="to bottom, rgba(255,255,255,1), rgba(0,0,0,.5)"></v-img>
         </template>
-        <v-list>
-          <!-- <v-list-item v-for="link in links" :key="link.text" @click="routeTo(link.route)"> -->
-          <v-list-item v-for="link in links" :key="link.text" :to="link.route">
-            <v-icon left>{{ link.icon }}</v-icon>
-            <v-list-item-title>{{ link.text }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
 
-      <v-btn @click="loginModal=true" text color="gray">
-        <v-icon left>exit_to_app</v-icon>
-        <span>Sign In</span>
-      </v-btn>
+        <router-link to="/home" text-decoration="none">
+          <v-toolbar-title>
+            <v-icon large class="mr-2">fa-graduation-cap</v-icon>
+            <span class="font-weight-light">Study</span>
+            <span>PRO</span>
+          </v-toolbar-title>
+        </router-link>
+        <v-spacer></v-spacer>
+        <v-btn href="/workspace" target="_blank" class="mt-1" text color="gray">
+          <v-icon left>open_in_new</v-icon>
+          <span>WorkSpace</span>
+        </v-btn>
+        <v-btn class="mt-1" @click="loginModal=true" text color="gray">
+          <v-icon left>exit_to_app</v-icon>
+          <span>Sign In</span>
+        </v-btn>
+        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
-      <!-- loginModal -->
-      <v-dialog v-model="loginModal" max-width="600px">
-        <v-card id="lgiModal" class="px-0 pt-0">
-          <v-card-title class="customTheme darken-2 white--text pb-3">
-            <span class="headline">Log In</span>
-          </v-card-title>
-          <v-card-text class="py-0 px-7">
-            <v-container class="pb-0">
-              <v-row>
-                <v-col cols="12" class="pb-0">
-                  <v-icon left>email</v-icon>
-                  <v-text-field label="E-mail" required type="email"></v-text-field>
-                </v-col>
-                <v-col cols="12" class="py-0">
-                  <v-text-field
-                    hint="비밀번호는 8자리 이상의 문자 + 숫자 조합입니다"
-                    label="Password"
-                    required
-                    type="password"
-                  ></v-text-field>
-                </v-col>
-                <v-checkbox
-                  id="modalCheckbox"
-                  class="py-0 mb-0"
-                  v-model="checkbox"
-                  label="로그인 상태 유지"
-                  color="primary"
-                  value="primary"
-                  hide-details
-                ></v-checkbox>
-              </v-row>
+        <!-- Menu Tab -->
+        <template v-slot:extension>
+          <v-tabs
+            align-with-title
+            background-color="transparent"
+            v-for="menu in menus"
+            :key="menu.title"
+          >
+            <v-tab :to="menu.route" class="mx-auto">
+              <v-icon left>{{ menu.icon }}</v-icon>
+              {{ menu.title }}
+            </v-tab>
+          </v-tabs>
+        </template>
+      </v-app-bar>
+    </v-card>
+
+    <!-- Login Modal Boxs -->
+    <v-dialog v-model="loginModal" max-width="600px">
+      <v-card id="lgiModal" class="px-0 pt-0">
+        <v-card-title class="customTheme darken-2 white--text pb-3">
+          <span class="headline">Log In</span>
+        </v-card-title>
+        <v-card-text class="py-0 px-7">
+          <v-container class="pb-0">
+            <v-row>
+              <v-col cols="12" class="pb-0">
+                <v-text-field required type="email">
+                  <template v-slot:label>
+                    <strong>
+                      <v-icon style="vertical-align: middle">email</v-icon>
+                    </strong> E-mail
+                  </template>
+                </v-text-field>
+              </v-col>
+              <v-col cols="12" class="py-0">
+                <v-text-field hint="비밀번호는 8자리 이상의 문자 + 숫자 조합입니다" required type="password">
+                  <template v-slot:label>
+                    <strong>
+                      <v-icon style="vertical-align: middle">lock</v-icon>
+                    </strong> Password
+                  </template>
+                </v-text-field>
+              </v-col>
+              <v-checkbox
+                id="modalCheckbox"
+                class="py-0 mb-0"
+                v-model="checkbox"
+                label="로그인 상태 유지"
+                color="primary"
+                value="primary"
+                hide-details
+              ></v-checkbox>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions class="pt-0 pr-5">
+          <v-spacer></v-spacer>
+          <v-btn color="error lighten-1" tile @click="loginModal= false">창 닫기</v-btn>
+          <v-btn color="customTheme darken-2 white--text" tile @click="loginModal= false">로그인</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <!-- Navigation Bar -->
+    <v-navigation-drawer app right v-model="drawer" class="customTheme lighten-1">
+      <div>
+        <v-layout column class="white">
+          <v-flex class="mt-2">
+            <v-container>
+              <v-icon large class="mr-5" @click="drawer = false">keyboard_arrow_right</v-icon>
+              <router-link to="/home" text-decoration="none">
+                <v-icon class="mr-2">fa-graduation-cap</v-icon>
+                <span class="font-weight-light">Study</span>
+                <span>PRO</span>
+              </router-link>
             </v-container>
-          </v-card-text>
-          <v-card-actions class="pt-0 pr-5">
-            <v-spacer></v-spacer>
-            <v-btn color="error lighten-1" tile @click="loginModal= false">창 닫기</v-btn>
-            <v-btn color="customTheme darken-2 white--text" tile @click="loginModal= false">로그인</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </v-toolbar>
+          </v-flex>
+        </v-layout>
+      </div>
 
-    <v-navigation-drawer app v-model="drawer" class="customTheme lighten-1">
+      <v-layout column align-center>
+        <v-flex class="mt-5">
+          <v-avatar size="100">
+            <img src="@/assets/articles/pigduck.jpg" alt />
+          </v-avatar>
+        </v-flex>
+        <v-flex>
+          <p class="white--text subheading mt-2">Kim Hyeon Cheol</p>
+        </v-flex>
+      </v-layout>
+
+      <v-divider class="black ma-3"/>
+
       <v-list>
-        <v-list-item v-for="link in links" :key="link.text" :to="link.route">
-          <v-list-item-action>
-            <v-icon class="white--text">{{ link.icon }}</v-icon>
-          </v-list-item-action>
+        <v-list-item v-for="item in items" :key="item.title" :to="item.route">
+          <v-list-item-icon>
+            <v-icon class="pink--text">{{ item.icon }}</v-icon>
+          </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+            <v-list-item-title class="white--text">{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
-    </v-navigation-drawer>
 
-    <!-- Expansion Panels -->
-    <!-- <v-container>
-      <v-expansion-panel>
-        <v-expansion-panel-content v-for="link in links" :key="link.text" router :to="link.route">
-          <div slot="header">{{ link.text }}</div>
-          <v-card>
-            <v-card-text>
-              <span class="font-weight-bold">HelloHello</span>
-              <span>Info</span>
-            </v-card-text>
-          </v-card>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-    </v-container>-->
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn block>Log out</v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
   </nav>
 </template>
 
@@ -112,12 +155,24 @@ export default {
       drawer: false,
       loginModal: false,
       checkbox: false,
-      links: [
-        { icon: "dashboard", text: "DashBoard", route: "/" },
-        { icon: "folder", text: "My Projects", route: "/home" },
-        { icon: "person", text: "Team", route: "/home" }
+      menus: [
+        { icon: "home", title: "Main", route: "/home" },
+        { icon: "group", title: "Study", route: "/study" },
+        { icon: "alarm", title: "Board", route: "/board" }
+      ],
+      items: [
+        { icon: "group", title: "My Study", route: "/study" },
+        { icon: "calendar_today", title: "Calendar", route: "/calendar" },
+        { icon: "account_circle", title: "Account", route: "/account" }
       ]
     };
   }
 };
 </script>
+
+<style scoped>
+.v-application a {
+  color: gray;
+  text-decoration: none;
+}
+</style>
