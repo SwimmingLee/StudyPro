6fda74b2091be53e486fa81ed1f88afe5388ca8a
KimHyeonCheol <supergrammer@naver.com>
2020-01-29 12:11:27 +0900

    [Feat] Board & Chat add, S02P12A106-21

--- front-sk/src/components/workspace/Board.vue
+++ front-sk/src/components/workspace/Board.vue
@@ -1,79 +1,86 @@
 <template>
-  <canvas id="canvas" width="640" height="640"></canvas>
+  <canvas
+    ref="canvas"
+    @mousedown="mouse_down"
+    @mouseup="mouse_up"
+    @mousemove="mouse_move"
+    @mouseout ="mouse_out"
+    id="canvas"
+    width="640"
+    height="640"
+  ></canvas>
 </template>
-
-<script src="//code.jquery.com/jquery-1.11.1.js"></script>
 
 <script>
 const io = require("socket.io-client");
-$(document).ready(function() {
-  function Point(event, target) {
-    // Point 생성자 함수 선언
-    this.x = event.pageX - $(target).position().left; // 파폭은 offsetX 속성을 지원하지 않으므로 이런 방식의 코드를 사용함
-    this.y = event.pageY - $(target).position().top;
+var socket = null;
+
+export default {
+  data() {
+    return {
+      width: 5,
+      color: "#000000",
+      isDown: false, // 마우스 버튼을 눌렀나 안 눌렀나
+      old_x: "",
+      old_y: "",
+      new_x: "",
+      new_y: ""
+      // canvas : document.getElementById("canvas"),
+      // context:'',        //Canvas 객체 추출
+    };
+  },
+  methods: {
+    mouse_down(event) {
+      this.isDown = true;
+      this.old_x = event.offsetX;
+      this.old_y = event.offsetY;
+      console.log(this.old_x, " ", this.old_y);
+    },
+    mouse_up() {
+      this.isDown = false;
+      console.log(this.old_x, " ", this.old_y);
+    },
+    mouse_move(event) {
+      if (this.isDown) {
+        this.new_x = event.offsetX;
+        this.new_y = event.offsetY;
+
+        socket.emit("draw", {
+          room_id: 1,
+          width: this.width,
+          color: this.color,
+          x1: this.old_x,
+          y1: this.old_y,
+          x2: this.new_x,
+          y2: this.new_y
+        });
+        this.old_x = this.new_x;
+        this.old_y = this.new_y;
+      }
+    },
+    mouse_out(){
+      this.isDown = false;
+    }
+  },
+  mounted() {
+    // console.log(this.$refs.canvas);
+    // this.ctx = this.$refs.canvas.getContext('2d');
+    this.canvas = document.getElementById("canvas");
+    this.context = this.canvas.getContext("2d");
+    socket.on("line", (data)=> {
+      this.context.lineWidth = data.width;
+      this.context.strokeStyle = data.color;
+      this.context.beginPath();
+      this.context.moveTo(data.x1, data.y1);
+      this.context.lineTo(data.x2, data.y2);
+      this.context.stroke();
+    });
+  },
+  created() {
+    socket = io.connect("http://70.12.246.89:8210", {
+      transports: ["websocket"]
+    });
+    socket.emit("join", 1);
   }
-  var canvas = document.getElementById("canvas"); // Canvas 객체 추출
-  var context = canvas.getContext("2d");
-
-  // 변수 선언
-  var width = 5;
-  var color = "#000000";
-  var isDown = false; // 마우스 버튼을 눌렀나 안 눌렀나
-  var newPoint, oldPoint;
-
-  // 이벤트 연결
-  canvas.onmousedown = function(event) {
-    isDown = true; // 눌렀다
-    oldPoint = new Point(event, this); // 현재 마우스 위치를 기록
-  };
-  canvas.onmouseup = function() {
-    isDown = false;
-  };
-  canvas.onmousemove = function(event) {
-    if (isDown) {
-      newPoint = new Point(event, this);
-
-      socket.emit("draw", {
-        room_id: 1,
-        width: width,
-        color: color,
-        x1: oldPoint.x,
-        y1: oldPoint.y,
-        x2: newPoint.x,
-        y2: newPoint.y
-      });
-
-      oldPoint = newPoint;
-    }
-  };
-
-  var socket = io.connect("http://70.12.246.89:8210", {
-    transports: ["websocket"]
-  }); // 소켓 이벤트를 연결
-  socket.emit("join", 1);
-  socket.on("line", function(data) {
-    context.lineWidth = data.width;
-    context.strokeStyle = data.color;
-    context.beginPath();
-    context.moveTo(data.x1, data.y1);
-    context.lineTo(data.x2, data.y2);
-    context.stroke();
-    console.log("Hello");
-  });
-
-  // UI를 구성
-  $("#colorpicker").farbtastic(function(data) {
-    // Farbtastic Color Picker
-    color = data;
-  });
-
-  $("#slider").slider({
-    max: 20,
-    min: 1,
-    value: 5,
-    change: function(event, ui) {
-      width = ui.value;
-    }
-  });
-});
+};
 </script>
