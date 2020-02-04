e33195064fe1076995922c4adceb3553a61a26d1
김은성 <unsung102@naver.com>
2020-02-04 15:32:35 +0900

    [Feat] Add view share beta

--- front/src/components/workspace/ViewShare.vue
+++ front/src/components/workspace/ViewShare.vue
@@ -1,5 +1,12 @@
 <template>
   <v-container>
+    
+    <!-- <p v-for="user in connected_users" :key="user" :value="user">{{user}}</p> -->
+    <select v-model="sharing_user_id" style = "border : 1px solid #000000">
+        <option disabled value="">Please select one</option>
+        <option :value="user_id">{{ user_id }}</option>
+        <option v-for="user in connected_users" :value="user" :key="user">{{user}}</option>
+    </select>
     <v-card>
       <video playsinline id="view_share" autoplay preload="metadata" width="640" height="640"></video>
     </v-card>
@@ -21,16 +28,26 @@ export default {
     return {
       view_share_video: null,
       local_stream: null,
+      sharing_user_id: null,
+      now_sharing: null,
 
-      peer_connections: {}
+      peer_connections: {},
+      received_pc: null,
     };
   },
-  props: ["socket", "user_id", "sharing_user_id"],
+  props: ["socket", "user_id", "connected_users"],
+  watch: {
+    sharing_user_id: function(change) {
+        if (change == this.now_sharing) return
+        console.log('change share =>', change)
+        this.socket.emit('viewsharestart', {user_id : change})
+    }
+  },
   methods: {
     async get_stream(stream) {
-      this.view_share_video = await document.getElementById("view_share");
+      
       this.local_stream = stream;
-      // this.view_share_video.srcObject = this.local_stream
+      this.view_share_video.srcObject = this.local_stream
     },
 
     sendMessage(message) {
@@ -43,7 +60,7 @@ export default {
         return this.peer_connections[user_id];
       }
       let t_pc = await new RTCPeerConnection(pcConfig);
-
+      console.log(t_pc)
       this.peer_connections[user_id] = t_pc;
 
       t_pc.onicecandidate = event => {
@@ -56,29 +73,67 @@ export default {
               candidate: event.candidate.candidate
             },
             study_id: 1,
-            from: this.user_id
+            from: this.user_id,
+            to: user_id
           });
         }
       };
 
       t_pc.onaddstream = event => {
-        this.view_share_video.srcObject = event.stream;
+        if (user_id != this.user_id) this.view_share_video.srcObject = event.stream;
         console.log(event.stream, "onaddstream");
       };
-
+      // if (user_id == this.user_id) t_pc.addStream(this.local_stream);
       t_pc.addStream(this.local_stream);
       return t_pc;
     }
   },
   mounted() {
-    navigator.mediaDevices
-      .getDisplayMedia({ video: true })
-      .then(this.get_stream);
+    this.view_share_video = document.getElementById("view_share");
+    navigator.mediaDevices.getUserMedia({audio:true})
+    .then(stream => {
+      this.local_stream = stream
+    })
+    this.socket.on('viewsharestart', sharing_user_id => {
+
+      console.log('view start', sharing_user_id)
+      for (let i in this.peer_connections) delete this.peer_connections[i]
+      this.received_pc = null
+      this.view_share_video.srcObject = null
+      this.now_sharing = sharing_user_id
+      console.log(sharing_user_id == this.user_id)
+      if (sharing_user_id == this.user_id) {
+        navigator.mediaDevices
+        .getDisplayMedia({ video: true })
+        .then(this.get_stream)
+        .then(() => {
+          for (let peer_id of this.connected_users) {
+            this.getPeerConnection(peer_id)
+            .then(t_pc => {
+              t_pc.createOffer(sdp => {
+                t_pc.setLocalDescription(sdp) 
+                this.sendMessage({
+                  message: sdp,
+                  study_id: 1,
+                  from: this.user_id,
+                  to: peer_id
+                })
+              }, e => console.log(e))
+              console.log('send offer to', peer_id )
+            })
+          }
+        });
+      }
+
+    })
+
 
     this.socket.on("join", data => {
-      if (this.user_id !== this.sharing_user_id) {
-        return;
-      }
+      this.peer_connections.push(data.user_id)
+
+      if (this.user_id != this.now_sharing) return
+      
+
       const user_id = data.user_id;
 
       setTimeout(() => {
@@ -89,7 +144,8 @@ export default {
               this.sendMessage({
                 message: sdp,
                 study_id: 1,
-                from: this.user_id
+                from: this.user_id,
+                to: user_id
               });
             },
             e => {
@@ -101,16 +157,17 @@ export default {
     });
 
     this.socket.on("leave", data => {
-      if (data.user_id === this.sharing_user_id) {
+      if (data.user_id == this.now_sharing) {
         this.view_share_video.srcObject = null;
       }
       delete this.peer_connections[data.user_id];
     });
 
-    this.socket.on("viewsharing", data => {
+    this.socket.on("viewshare", data => {
+      console.log(data)
       if (data.message.type === "offer") {
         const from = data.from;
-
+        console.log('get offer')
         this.getPeerConnection(from).then(t_pc => {
           t_pc.setRemoteDescription(new RTCSessionDescription(data.message));
           t_pc.createAnswer().then(sdp => {
@@ -118,12 +175,14 @@ export default {
             this.sendMessage({
               message: sdp,
               study_id: 1,
-              from: this.user_id
+              from: this.user_id,
+              to: from
             });
           });
         });
       } else if (data.message.type === "answer") {
         let t_pc = this.peer_connections[data.from];
+        console.log('get answer')
         t_pc.setRemoteDescription(new RTCSessionDescription(data.message));
       } else if (data.message.type === "candidate") {
         let candidate = new RTCIceCandidate({
