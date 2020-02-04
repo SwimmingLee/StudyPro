50cd5e413b491ee45365af87595f5c519d41e248
KimHyeonCheol <supergrammer@naver.com>
2020-02-04 16:12:21 +0900

    Hello

---
+++ front/src/components/board/board_study.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container flex>
+    <v-flex class="ma-2 mb-5">
+      <v-icon large class="mr-2" color="black">menu_book</v-icon>스터디 게시판
+    </v-flex>
+    <v-row justify="center">
+      <v-expansion-panels class="pa-3" multiple popout>
+        <v-expansion-panel v-for="(item, index) in 10" :key="index">
+          <v-expansion-panel-header>Content {{ item }}</v-expansion-panel-header>
+          <v-expansion-panel-content>Hello Hello Content {{ item }}</v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-row>
+
+    <v-row justify="center" class="ma-5">
+      <v-pagination v-model="page" :length="15" :total-visible="9"></v-pagination>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+// import VueAxious from 'vue-axios'
+
+export default {
+  data() {
+    return {
+      page: 1,
+      lastpage: 1
+    };
+  },
+
+  mounted() {
+    axios
+      .get("http://localhost:8000/posts/list?board=study")
+      .then(function(res) {
+        console.log(res);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+};
+</script>
