bc39c1074f79eb11851995dec18e826e03e9f6eb
SwimmingLee <swimming_7291@daum.net>
2020-02-04 16:12:39 +0900

    [fix] modify ok

--- front/src/services/user.service.js
+++ front/src/services/user.service.js
@@ -1,20 +1,36 @@
 import axios from 'axios'
 import authHeader from './auth-header'
+import dotenv from "dotenv"
 
-const API_URL = 'http://15.164.245.201:8000/users/'
+dotenv.config();
 
 class UserService {
     getUserContent() {
-        return axios.get(API_URL + "token", { headers: authHeader() })
+        return axios.get(process.env.API_URL + "token", { headers: authHeader() })
     }
 
-    
+
     getUserContentById(payload) {
-        return axios.get(API_URL + "/:uid", 
+        return axios.get(process.env.API_URL + "/:uid", 
             {data:{
                 uid: payload.uid  
         }})
     }
+
+    requestSocialLogin = (email, nickname, gender, platform) => {
+        return axios({
+            method: 'POST',
+            url: 'http://15.164.245.201:8000/users/social/signin',
+            data: {
+                'email': email,
+                'nickname':nickname,
+                'gender': gender,
+                'platform_type': platform
+            }
+        })
+    }
+
+    
 }
 
 export default new UserService()
