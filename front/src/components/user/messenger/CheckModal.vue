81680909f1e84ef5fbe03db9069220227d7e11e2
cyun <dawnstar1007@naver.com>
2020-02-07 09:25:03 +0900

    [Fix] 쪽지 전송 재확인 수정, S02P13A106-29

--- front/src/components/user/messenger/CheckModal.vue
+++ front/src/components/user/messenger/CheckModal.vue
@@ -2,7 +2,6 @@
   <div id="checkmodal">
     <v-dialog v-model="subopen" persistent max-width="40%">
       <v-card class="px-0 pt-0">
-
         <v-card-text style="font-size:15px" align="center" class="pa-5">
           메시지를 전송하시겠습니까?
         </v-card-text>
@@ -37,19 +36,15 @@
       </v-card>
     </v-dialog>
 
-
-
     <v-dialog v-model="subalert" max-width="40%">
-      <v-card v-if="this.isSuccess==true" class="px-0 pt-0">
-
+      <v-card v-if="this.isSuccess == true" class="px-0 pt-0">
         <v-card-text style="font-size:15px" align="center" class="pa-5">
           메시지를 성공적으로 전송했습니다.
         </v-card-text>
 
-
         <v-card-actions class="pt-0 pr-5">
           <v-spacer></v-spacer>
-          
+
           <v-btn
             color="error--text lighten-1 transparent"
             elevation="0"
@@ -59,16 +54,14 @@
         </v-card-actions>
       </v-card>
 
-      <v-card v-if="this.isSuccess==false" class="px-0 pt-0">
-
+      <v-card v-if="this.isSuccess == false" class="px-0 pt-0">
         <v-card-text style="font-size:15px" align="center" class="pa-5">
           메시지 전송에 실패했습니다.
         </v-card-text>
 
-
         <v-card-actions class="pt-0 pr-5">
           <v-spacer></v-spacer>
-          
+
           <v-btn
             color="error--text lighten-1 transparent"
             elevation="0"
@@ -77,11 +70,7 @@
           >
         </v-card-actions>
       </v-card>
-
-
-
     </v-dialog>
-
   </div>
 </template>
 
@@ -90,10 +79,10 @@ export default {
   name: "checkmodal",
   data: () => ({
     subopen: false,
-    subalert:false,
-    isSuccess:true,
+    subalert: false,
+    isSuccess: true
   }),
-  props: ["checkModal","item","titleText","mainText"],
+  props: ["checkModal", "item", "titleText", "mainText"],
   watch: {
     checkModal() {
       this.subopen = this.checkModal;
@@ -108,46 +97,43 @@ export default {
     clickNo() {
       this.$emit("sendNo");
     },
-    sendSuccess(){
-        this.subalert = false;
-        // this.checkModal=false;
-          this.$emit("sendYes");
-         
+    sendSuccess() {
+      this.subalert = false;
+      // this.checkModal=false;
 
+      this.$emit("sendYes");
     },
-    sendFail(){
-          this.$emit("sendYes");
-          this.checkModal=false;
-
-
+    sendFail() {
+      this.subalert = false;
+      this.$emit("sendNo");
+      // this.checkModal=false;
     },
 
-    async clickYes(){
+    async clickYes() {
+      //통신 시도
 
-        //통신 시도
+      this.subalert = true;
+      this.subopen = false;
 
-        this.subalert=true;
+      // 통신 성공
+
+      this.isSuccess = true;
+
+      if (this.isSuccess) {
         this.subopen = false;
-
-        // //통신 성공
-        // if(this.isSuccess){
-
-        //     this.subopen= false;
-        //      this.$emit("sendYes")
-
-        // }
-
-        //  //통신 실패
-        // else if(!this.isSuccess){
-        //      this.$emit("sendNo")
-
-        // }
+        this.$emit("sendYes");
 
 
 
-        // this.$emit("sendYes")
+      }
 
+      //  //통신 실패
+      // else if(!this.isSuccess){
+      //      this.$emit("sendNo")
 
+      // }
+
+      // this.$emit("sendYes")
     }
   }
 };
