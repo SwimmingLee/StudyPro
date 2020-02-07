73d664e6941421b840ba0b7bbdeef782183044c3
cyun <dawnstar1007@naver.com>
2020-02-07 15:31:13 +0900

    수정

--- front/src/components/user/messenger/MessageHome.vue
+++ front/src/components/user/messenger/MessageHome.vue
@@ -3,20 +3,20 @@
     <v-container>
       <div class="tabs">
         <v-toolbar flat color="black" dark>
-            <v-toolbar-title style="font-size:30px">쪽지함</v-toolbar-title>
-          </v-toolbar>
+          <v-toolbar-title style="font-size:30px">쪽지함</v-toolbar-title>
+        </v-toolbar>
         <v-tabs v-model="tab">
           <v-tab>받은 쪽지함</v-tab>
           <v-tab>보낸 쪽지함</v-tab>
           <v-tab>주소록</v-tab>
           <v-tab-item>
-            <rx-box :tab="tab"/>
+            <rx-box :tab="tab" />
           </v-tab-item>
           <v-tab-item>
-            <tx-box :tab="tab"/>
+            <tx-box :tab="tab" />
           </v-tab-item>
           <v-tab-item>
-            <friends-list :tab="tab"/>
+            <friends-list :tab="tab" />
           </v-tab-item>
         </v-tabs>
       </div>
@@ -27,8 +27,8 @@
 export default {
   data: () => {
     return {
-      tab: null,
-    }
+      tab: null
+    };
   },
   computed: {
     isAuth: function() {
@@ -40,7 +40,6 @@ export default {
     TxBox: () => import("@/components/user/messenger/TxBox"),
     FriendsList: () => import("@/components/user/messenger/FriendsList")
   },
-  methods: {
-  }
+  methods: {}
 };
 </script>
