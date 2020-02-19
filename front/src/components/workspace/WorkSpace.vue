2d4db6163bc865e7a184ac8412e22aea57517f5f
Ubuntu <ubuntu@ip-172-26-9-23.ap-northeast-2.compute.internal>
2020-02-19 07:25:49 +0000

    [modify] workController

--- front/src/components/workspace/WorkSpace.vue
+++ front/src/components/workspace/WorkSpace.vue
@@ -188,12 +188,8 @@ export default {
         };
     this.study_id = window.location.href.split("workspace/")[1];
     this.socket = io.connect(
-      `http://70.12.247.73:8210/?study_id=${this.study_id}&user_id=${this.user.user_id}&user_nickname=${this.user.user_nickname}`,
-      // this.socket = io.connect(
-      //   `http://70.12.246.89:8210/?study_id=${this.study_id}&user_id=${this.user.user_id}&user_nickname=${this.user.user_nickname}`,
+        process.env.VUE_APP_SOCKET_URL + `${this.study_id}&user_id=${this.user.user_id}&user_nickname=${this.user.user_nickname}`,
       {
-        // this.socket = io.connect(`https://15.164.245.201:8210/?study_id=${this.study_id}&user_id=${this.user_id}`, {
-        // this.socket = io.connect(`https://i02a106.p.ssafy.io:8210/?study_id=${this.study_id}&user_id=${this.user_id}`, {
         transports: ["websocket"],
         secure: true
       }
